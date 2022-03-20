b77e62e7e2b28a81695f3be3491e81f49f5d960b
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-03-20 17:14:53 +0900

    feat: post Resolverのモック実装

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PostsModule } from './components/posts/posts.module';
+import * as path from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    PostsModule,
+  ],
 })
 export class AppModule {}
